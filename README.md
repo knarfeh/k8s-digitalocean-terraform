b2c6e293acd4e9bd825af4058ddcb393d125a4f8
knarfeh <knarfeh@outlook.com>
2018-03-28 11:49:34 +0800

    Init, cp from Mosho1
    03ad77859b2fb6714490fd75240a4e644012

---
+++ README.md
@@ -0,0 +1,178 @@
+# Kubernetes - DigitalOcean - Terraform
+
+Deploy your Kubernetes cluster on DigitalOcean using Terraform.
+
+## Requirements
+
+* [DigitalOcean](https://www.digitalocean.com/) account
+* DigitalOcean Token [In DO's settings/tokens/new](https://cloud.digitalocean.com/settings/tokens/new)
+* [Terraform](https://www.terraform.io/)
+* CloudFlare's PKI/TLS toolkit [cfssl](https://github.com/cloudflare/cfssl)
+
+### On Mac
+
+With brew installed, all tools can be installed with
+
+```bash
+brew install terraform cfssl kubectl 
+```
+
+Do all the following steps from a development machine. It does not matter _where_ it is, as long as it is connected to the internet. This one will be subsequently used to access the cluster via `kubectl`.
+
+## Generate private / public keys
+
+```
+ssh-keygen -t rsa -b 4096
+```
+
+The system will prompt you for a file path to save the key, we will go with `~/.ssh/id_rsa` in this tutorial.
+
+## Add your public key in the DigitalOcean control panel
+
+[Do it here](https://cloud.digitalocean.com/settings/security). Name it and paste the public key just below `Add SSH Key`.
+
+## Add this key to your SSH agent
+
+```bash
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+```
+
+## Invoke Terraform
+
+We put our DigitalOcean token in the file `./secrets/DO_TOKEN` (this directory is mentioned in `.gitignore`, of course, so we don't leak it)
+
+Then we setup the environment variables (step into `this repository` root). Note that the first variable sets up the *number of workers*
+
+```bash
+export TF_VAR_number_of_workers=3
+export TF_VAR_do_token=$(cat ./secrets/DO_TOKEN)
+export TF_VAR_ssh_fingerprint=$(ssh-keygen -E MD5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | sed 's/MD5://g')
+```
+
+If you are using an older version of OpenSSH (<6.9), replace the last line with
+```bash
+export TF_VAR_ssh_fingerprint=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+```
+
+There is a convenience script for you in `./setup_terraform.sh`. Invoke it as
+
+```bash
+. ./setup_terraform.sh
+```
+
+Optionally, you can customize the datacenter *region* via:
+```bash
+export TF_VAR_do_region=fra1
+```
+The default region is `nyc3`. You can find a list of available regions from [DigitalOcean](https://developers.digitalocean.com/documentation/v2/#list-all-regions).
+
+After setup, call `terraform apply`
+
+```bash
+terraform apply
+```
+
+That should do! `kubectl` is configured, so you can just check the nodes (`get no`) and the pods (`get po`).
+
+```bash
+$ kubectl get no
+NAME          LABELS                               STATUS
+X.X.X.X   kubernetes.io/hostname=X.X.X.X   Ready     2m
+Y.Y.Y.Y   kubernetes.io/hostname=Y.Y.Y.Y   Ready     2m
+
+$ kubectl --namespace=kube-system get po
+NAME                                   READY     STATUS    RESTARTS   AGE
+kube-apiserver-X.X.X.X                    1/1       Running   0          13m
+kube-controller-manager-X.X.X.X           1/1       Running   0          12m
+kube-proxy-X.X.X.X                        1/1       Running   0          12m
+kube-proxy-X.X.X.X                        1/1       Running   0          11m
+kube-proxy-X.X.X.X                        1/1       Running   0          12m
+kube-scheduler-X.X.X.X                    1/1       Running   0          13m
+```
+
+You are good to go. Now, we can keep on reading to dive into the specifics.
+
+## Deploy details
+
+These scripts are mostly taken from the [CoreOS + Kubernetes Step by Step](https://coreos.com/kubernetes/docs/latest/getting-started.html) guide, with the addition of SSL/TLS and client certificate authentication for etcd2. 
+
+Certificate generation is covered in more detail by CoreOS's [Generate self-signed certificates](https://coreos.com/os/docs/latest/generate-self-signed-certificates.html) documentation.
+
+These resources are excellent starting places for more in-depth documentation. Below is an overview of the cluster.
+
+### K8s etcd
+
+A dedicated host running a TLS secured + authenticated etcd2 instance for Kubernetes.
+
+#### Cloud config
+
+See the template `00-etcd.yaml`.
+
+### K8s master
+
+The cluster master, running:
+
+* flanneld
+* kubelet
+* kube-proxy
+* kube-apiserver
+* kube-controller-manager
+* kube-scheduler
+
+#### Cloud config
+
+See the template `01-master.yaml`.
+
+#### Provisions
+
+Once we create this droplet (and get its `IP`), the TLS assets will be created locally (i.e. on the development machine from which we run `terraform`), and put into the directory `secrets` (which, again, is mentioned in `.gitignore`). The TLS assets consist of a server key and certificate for the API server, as well as a client key and certificate to authenticate flanneld and the API server to etcd2.
+
+The TLS assets are copied to appropriate directories on the K8s master using Terraform `file` and `remote-exec` provisioners.
+
+Lastly, we start and enable both `kubelet` and `flanneld`, and finally create the `kube-system` namespace.
+
+### K8s workers
+
+Cluster worker nodes, each running:
+
+* flanneld
+* kubelet
+* kube-proxy
+* docker
+
+#### Cloud config
+
+See the template `02-worker.yaml`.
+
+#### Provisions
+
+For each droplet created, a TLS client key and certificate will be created locally (i.e. on the development machine from which we run `terraform`), and put into the directory `secrets` (which, again, is mentioned in `.gitignore`). 
+
+The TLS assets are then copied to appropriate directories on the worker using Terraform `file` and `remote-exec` provisioners.
+
+Finally, we start and enable `kubelet` and `flanneld`.
+
+### Setup `kubectl`
+
+After the installation is complete, `terraform` will configure `kubectl` for you. The environment variables will be stored in the file `secrets/setup_kubectl.sh`.
+
+Test your brand new cluster
+
+```bash
+kubectl get nodes
+```
+
+You should get something similar to
+
+```
+$ kubectl get nodes
+NAME          LABELS                               STATUS
+X.X.X.X       kubernetes.io/hostname=X.X.X.X       Ready
+```
+
+### Deploy microbot with External IP
+
+The file `04-microbot.yaml` will be rendered (i.e. replace the value `EXT_IP1`), and then `kubectl` will create the Service and Replication Controller.
+
+To see the IP of the service, run `kubectl get svc` and look for the `EXTERNAL-IP` (should be the first worker's ext-ip).
---
+++ deploy.tf
@@ -0,0 +1,208 @@
+###############################################################################
+#
+# A simple K8s cluster in DO
+#
+###############################################################################
+
+
+###############################################################################
+#
+# Get variables from command line or environment
+#
+###############################################################################
+
+
+variable "do_token" {}
+variable "do_region" {
+    default = "nyc3"
+}
+variable "ssh_fingerprint" {}
+variable "ssh_private_key" {
+    default = "~/.ssh/id_rsa"
+}
+
+variable "number_of_workers" {}
+variable "hyperkube_version" {
+    default = "v1.8.4_coreos.0"
+}
+
+variable "prefix" {
+    default = ""
+}
+
+variable "size_master" {
+    default = "2gb"
+}
+
+variable "size_worker" {
+    default = "2gb"
+}
+
+
+###############################################################################
+#
+# Specify provider
+#
+###############################################################################
+
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+
+###############################################################################
+#
+# Master host
+#
+###############################################################################
+
+
+resource "digitalocean_droplet" "k8s_master" {
+    image = "coreos-stable"
+    name = "${var.prefix}k8s-master"
+    region = "${var.do_region}"
+    private_networking = true
+    size = "${var.size_master}"
+    ssh_keys = ["${split(",", var.ssh_fingerprint)}"]
+
+    provisioner "file" {
+        source = "./00-master.sh"
+        destination = "/tmp/00-master.sh"
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    provisioner "file" {
+        source = "./install-kubeadm.sh"
+        destination = "/tmp/install-kubeadm.sh"
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    # Install dependencies and set up cluster
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/install-kubeadm.sh",
+            "sudo /tmp/install-kubeadm.sh",
+            "export MASTER_PRIVATE_IP=\"${digitalocean_droplet.k8s_master.ipv4_address_private}\"",
+            "export MASTER_PUBLIC_IP=\"${digitalocean_droplet.k8s_master.ipv4_address}\"",
+            "chmod +x /tmp/00-master.sh",
+            "sudo -E /tmp/00-master.sh"
+        ]
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    # copy secrets to local
+    provisioner "local-exec" {
+        command =<<EOF
+            scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.ssh_private_key} core@${digitalocean_droplet.k8s_master.ipv4_address}:"/tmp/kubeadm_join /etc/kubernetes/admin.conf" ${path.module}/secrets/
+            sed -i '' "s/${digitalocean_droplet.k8s_master.ipv4_address_private}/${digitalocean_droplet.k8s_master.ipv4_address}/" ${path.module}/secrets/admin.conf
+EOF
+    }
+
+}
+
+
+###############################################################################
+#
+# Worker hosts
+#
+###############################################################################
+
+
+resource "digitalocean_droplet" "k8s_worker" {
+    count = "${var.number_of_workers}"
+    image = "coreos-stable"
+    name = "${var.prefix}${format("k8s-worker-%02d", count.index + 1)}"
+    region = "${var.do_region}"
+    size = "${var.size_worker}"
+    private_networking = true
+    # user_data = "${data.template_file.worker_yaml.rendered}"
+    ssh_keys = ["${split(",", var.ssh_fingerprint)}"]
+    depends_on = ["digitalocean_droplet.k8s_master"]
+
+    # Start kubelet
+    provisioner "file" {
+        source = "./01-worker.sh"
+        destination = "/tmp/01-worker.sh"
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    provisioner "file" {
+        source = "./install-kubeadm.sh"
+        destination = "/tmp/install-kubeadm.sh"
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    provisioner "file" {
+        source = "./secrets/kubeadm_join"
+        destination = "/tmp/kubeadm_join"
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+
+    # Install dependencies and join cluster
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/install-kubeadm.sh",
+            "sudo /tmp/install-kubeadm.sh",
+            "export NODE_PRIVATE_IP=\"${digitalocean_droplet.k8s_worker.ipv4_address}\"",
+            "chmod +x /tmp/01-worker.sh",
+            "sudo -E /tmp/01-worker.sh"
+        ]
+        connection {
+            type = "ssh",
+            user = "core",
+            private_key = "${file(var.ssh_private_key)}"
+        }
+    }
+}
+
+# use kubeconfig retrieved from master
+
+resource "null_resource" "deploy_microbot" {
+    depends_on = ["digitalocean_droplet.k8s_worker"]
+    provisioner "local-exec" {
+        command = <<EOF
+            export KUBECONFIG=${path.module}/secrets/admin.conf
+            sed -e "s/\$EXT_IP1/${digitalocean_droplet.k8s_worker.0.ipv4_address}/" < ${path.module}/02-microbot.yaml > ./secrets/02-microbot.rendered.yaml
+            until kubectl get pods 2>/dev/null; do printf '.'; sleep 5; done
+            kubectl create -f ./secrets/02-microbot.rendered.yaml
+EOF
+    }
+}
+
+resource "null_resource" "deploy_digitalocean_cloud_controller_manager" {
+    depends_on = ["digitalocean_droplet.k8s_worker"]
+    provisioner "local-exec" {
+        command = <<EOF
+            export KUBECONFIG=${path.module}/secrets/admin.conf
+            sed -e "s/\$DO_ACCESS_TOKEN/${var.do_token}/" < ${path.module}/03-do-secret.yaml > ./secrets/03-do-secret.rendered.yaml
+            until kubectl get pods 2>/dev/null; do printf '.'; sleep 5; done
+            kubectl create -f ./secrets/03-do-secret.rendered.yaml
+            kubectl create -f https://raw.githubusercontent.com/digitalocean/digitalocean-cloud-controller-manager/master/releases/v0.1.3.yml
+EOF
+    }
+}
