fcd285beee777113773efc522be01db948affa02
knarfeh <knarfeh@outlook.com>
2018-07-14 00:54:13 +0800

    fix: Latest helm

--- 00-master.sh
+++ 00-master.sh
@@ -15,8 +15,8 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl create namespace tiller-deploy
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 helm init
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
 # used to setup kubectl 
 # chown core /etc/kubernetes/admin.conf
